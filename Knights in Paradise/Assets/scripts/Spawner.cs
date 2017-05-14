c87f8ffaa5fa8a967e4dd95cbe90e1c5dc55b366
Jonathan <jonathan.daley@my.jcu.edu.au>
2017-05-14 11:14:21 +1000

    Create some items and spawning system.
    Created palm trees, coconuts and a system to spawn the coconut.
    Needs refimenment.

---
+++ Knights in Paradise/Assets/scripts/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+    public Transform[] spawnLocations;
+    public GameObject[] itemSpawn;
+    public GameObject[] itemClone;
+    public float spawnTime = 5f;
+    public float spawnDelay = 3f;
+
+    private void Start()
+    {
+        InvokeRepeating("spawnObject", spawnDelay, spawnTime);
+    }
+
+    void spawnObject()
+    {
+        for (int i = 0; i < itemSpawn.Length; i++)
+        {
+            itemClone[i] = Instantiate(itemSpawn[i], spawnLocations[i].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+        }
+    }
+
+}
